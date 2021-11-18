91944b6ca8079347177dc4dcd7fb9f01f26e908a
Daniel Reintanz <daniel.reintanz@gmail.com>
2021-11-18 11:57:36 +0100

    ADDED style.css REFACTORED relilab-termine.php logic into new class RelilabTermine single-termin-block.php removed unnecessary wordpress comments

--- relilab-termine.php
+++ relilab-termine.php
@@ -2,65 +2,71 @@
 /**
  *Plugin Name: relilab Termine
  */
-add_shortcode('relilab_termine','termineAusgeben');
 
-function   termineAusgeben( $atts ) {
-
-    $posts = get_posts(array(
-        'post_type'			=> 'post',
-        'posts_per_page'	=> -1,
-        'category'          => 'termine',
-        'meta_key'			=> 'relilab_startdate',
-        'orderby'			=> 'meta_value',
-        'order'				=> 'ASC'
-    ));
-    global $post;
-    $lastPostMonth = '';
-    ?>
-    <ul>
-    <?php
-    foreach ($posts as $post) {
-        setup_postdata( $post );
-        if (!empty(get_post_meta($post->ID, 'relilab_startdate',true)) && !empty(get_post_meta($post->ID, 'relilab_enddate',true)))
-        {
-            if ($template = locate_template('relilab-Termine'))
-                load_template($template);
-            else
-//            load_template('templates/single_termin_block.php');
-                include 'templates/single-termin-block.php';
-        }
+class RelilabTermine{
+    public function __construct(){
+        add_shortcode('relilab_termine',array($this,'termineAusgeben'));
+        add_action( 'wp_enqueue_scripts', array($this,'enqueue_scripts'));
     }
-    ?>
-
+    function   termineAusgeben( $atts )
+    {
+        $posts = get_posts(array(
+            'post_type'			=> 'post',
+            'posts_per_page'	=> -1,
+            'category'          => 'termine',
+            'meta_key'			=> 'relilab_startdate',
+            'orderby'			=> 'meta_value',
+            'order'				=> 'ASC'
+        ));
+        global $post;
+        ob_start();
+        ?>
+        <ul>
+            <?php
+            foreach ($posts as $post) {
+                setup_postdata( $post );
+                if (!empty(get_post_meta($post->ID, 'relilab_startdate',true)) && !empty(get_post_meta($post->ID, 'relilab_enddate',true)))
+                {
+                    if ($template = locate_template('relilab-Termine'))
+                        load_template($template);
+                    else
+                        load_template(dirname(__FILE__) . '/templates/single-termin-block.php',false);
+                }
+            }
+            ?>
         </ul>
-    <?php
-    wp_reset_postdata();
-}
-
-/**
- * @param string $date
- * @return string
- */
-function getWochentag(string $date): string
-{
-    $wochentag = array(     'Mon' => 'Montag',
-                            'Tue' => 'Dienstag',
-                            'Wed' => 'Mittwoch',
-                            'Thu' => 'Donnerstag',
-                            'Fri' => 'Freitag',
-                            'Sat' => 'Samstag',
-                            'Sun' => 'Sonntag',
+        <?php
+        wp_reset_postdata();
+        return ob_get_clean();
+    }
+    function enqueue_scripts() {
+     //   wp_enqueue_script( 'custom-js', plugin_dir_url( __FILE__ ) . 'js/custom.js', array( 'jquery' ), '', true );
+        wp_enqueue_style( 'single-termin-block-style', plugin_dir_url( __FILE__ ) . 'css/style.css' );
+    }
+    /**
+     * @param string $date
+     * @return string
+     */
+    static function getWochentag(string $date): string
+    {
+        $wochentag = array(     'Mon' => 'Montag',
+            'Tue' => 'Dienstag',
+            'Wed' => 'Mittwoch',
+            'Thu' => 'Donnerstag',
+            'Fri' => 'Freitag',
+            'Sat' => 'Samstag',
+            'Sun' => 'Sonntag',
         );
-    return $wochentag[date('D',strtotime($date))];
-}
+        return $wochentag[date('D',strtotime($date))];
+    }
 
-/**
- * @param string $date
- * @return string
- */
-function getMonat(string $date): string
-{
-    $monat = array(
+    /**
+     * @param string $date
+     * @return string
+     */
+    static function getMonat(string $date): string
+    {
+        $monat = array(
             'Jan'   => 'Januar',
             'Feb'   => 'Februar',
             'Mar'   => 'März',
@@ -73,9 +79,13 @@ function getMonat(string $date): string
             'Okt'   => 'Oktober',
             'Nov'   => 'November',
             'Dec'   => 'Dezember',
-    );
-    if(!empty($date))
-        return $monat[date('M',strtotime($date))];
-    else
-        return '';
+        );
+        if(!empty($date))
+            return $monat[date('M',strtotime($date))];
+        else
+            return '';
+    }
+
 }
+
+$object = new RelilabTermine();
--- templates/single-termin-block.php
+++ templates/single-termin-block.php
@@ -1,37 +1,26 @@
-<!-- wp:columns {"backgroundColor":"palette-color-5"} -->
-<div class="wp-block-columns has-palette-color-5-background-color has-background"><!-- wp:column {"verticalAlignment":"center","width":"200px"} -->
-    <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:200px"><!-- wp:group {"className":"relilab_termin_day"} -->
-        <div class="wp-block-group relilab_termin_day"><!-- wp:paragraph {"align":"center"} -->
+<div class="wp-block-columns has-palette-color-5-background-color has-background">
+    <div class="wp-block-column is-vertically-aligned-top" style="flex-basis:200px">
+        <div class="wp-block-group relilab_termin_day">
             <p class="has-text-align-center">zur Live Veranstaltung</p>
-            <!-- /wp:paragraph -->
 
-            <!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"40px"}}} -->
             <p class="has-text-align-center" style="font-size:40px"><?php echo date('j',strtotime(get_post_meta(get_the_ID(), 'relilab_startdate',true))).'.'; ?></p>
-            <!-- /wp:paragraph -->
 
-            <!-- wp:paragraph {"align":"center"} -->
-            <p class="has-text-align-center"></p>
-            <!-- /wp:paragraph --></div>
-        <!-- /wp:group -->
-
-        <!-- wp:heading {"textAlign":"center","level":5} -->
         <h5 class="has-text-align-center">
-            <?php echo getMonat(get_post_meta(get_the_ID(), 'relilab_startdate',true)) .' '. date('Y', strtotime(get_post_meta(get_the_ID(),'relilab_startdate', true))); ?>
+             <?php echo RelilabTermine::getMonat(get_post_meta(get_the_ID(), 'relilab_startdate',true)) .' '. date('Y', strtotime(get_post_meta(get_the_ID(),'relilab_startdate', true))); ?>
+
+
         </h5>
-        <!-- /wp:heading -->
 
-        <!-- wp:paragraph {"align":"center"} -->
-        <p class="has-text-align-center" ><?php echo getWochentag(get_post_meta(get_the_ID(), 'relilab_startdate',true)); ?></p>
-        <!-- /wp:paragraph -->
 
-        <!-- wp:paragraph {"align":"center"} -->
-        <p class="has-text-align-center"><?php echo date('H:i',strtotime(get_post_meta(get_the_ID(),'relilab_startdate',true))) .' - '. date('H:i',strtotime(get_post_meta(get_the_ID(),'relilab_enddate',true)))?></p>
-        <!-- /wp:paragraph -->
+            <p class="has-text-align-center"><?php echo RelilabTermine::getWochentag(get_post_meta(get_the_ID(), 'relilab_startdate',true)); ?></p>
+
+            <p class="has-text-align-center"><?php echo date('H:i',strtotime(get_post_meta(get_the_ID(),'relilab_startdate',true))) .' - '. date('H:i',strtotime(get_post_meta(get_the_ID(),'relilab_enddate',true)))?></p>
+
+        </div>
+
 
     </div>
-    <!-- /wp:column -->
 
-    <!-- wp:column -->
     <div class="wp-block-column">
         <?php if ( has_post_thumbnail() ) : ?>
             <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
@@ -41,8 +30,6 @@
         <br>
         <a class="has-text-align-center" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 
-        <!-- wp:paragraph -->
-        <p><?php the_excerpt(); ?>/p>
-        <!-- /wp:paragraph --></div>
-    <!-- /wp:column --></div>
-<!-- /wp:columns -->
+        <p><?php echo substr(get_the_excerpt(),0 ,150). ' ...'; ?>  </p>
+    </div>
+</div>
