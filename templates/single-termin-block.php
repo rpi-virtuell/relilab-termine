b9144b8c4f3eb9e9d5538c73a30126ff5c51ab05
FreelancerAMP <daniel.reintanz@gmail.com>
2021-12-07 13:23:22 +0100

    CHANGES: REFACTORED HTML styles into CSS

--- templates/single-termin-block.php
+++ templates/single-termin-block.php
@@ -3,8 +3,8 @@
 </div>
 
 
-<div class="wp-block-columns relilab_termin_box has-background" style="display:flex; justify-content: space-around">
-    <div class="wp-block-column" style=" flex: 0 0 175px; align-self: center">
+<div class="wp-block-columns relilab_termin_box has-background">
+    <div class="wp-block-column relilab_termin_button">
         <div class="wp-block-group relilab_termin_day"
              onclick="location.href='<?php echo get_option('options_relilab_zoom_link') ?>'">
             <div style="margin: 5px 5px">
@@ -17,13 +17,13 @@
         </div>
     </div>
 
-    <div class="wp-block-column relilab_termin_content" style="flex: 1 0 300px">
+    <div class="wp-block-column relilab_termin_content">
         <h3 class="entry-title"><a class="has-text-align-center"
                                    href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
         <p><?php echo substr(get_the_excerpt(), 0, 150) . ' ...'; ?>  </p>
     </div>
 
-    <div class="wp-block-column" style="flex: 0 1 200px">
+    <div class="wp-block-column relilab_termin_thumbnail">
         <?php if (has_post_thumbnail()) : ?>
 
             <div class="wp-block-image">
