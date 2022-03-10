2c1efae4057914389a309da3f732e128cce90aef
Daniel Reintanz <85614916+FreelancerAMP@users.noreply.github.com>
2022-03-10 09:43:44 +0100

    CHANGES: FIXED ADDED better case handling

--- relilab-termine.php
+++ relilab-termine.php
@@ -28,7 +28,13 @@ class RelilabTermine
         $termineId = get_category_by_slug('termine')->term_id;
         $termList = array_merge([$termineId], get_term_children($termineId, 'category'));
         if (has_term($termList, 'category', $id)) {
-            $content = "<p>" . get_post_meta($id, "relilab_startdate", true) . " - " . get_post_meta($id, "relilab_enddate", true) . " <a href='" . !empty(get_post_meta(get_the_ID(), "relilab_custom_zoom_link", true)) ? get_post_meta(get_the_ID(), "relilab_custom_zoom_link", true) : get_option("options_relilab_zoom_link") . "'>Zoom Link</a> </p>$content";
+            if (!empty(get_post_meta(get_the_ID(), "relilab_custom_zoom_link", true)) )
+            {
+                $zoom_link = get_post_meta(get_the_ID(), "relilab_custom_zoom_link", true);
+            }else{
+                $zoom_link =  get_option("options_relilab_zoom_link");
+            }
+            $content = "<p>" . get_post_meta($id, "relilab_startdate", true) . " - " . get_post_meta($id, "relilab_enddate", true) . " <a href='" . $zoom_link  . "'>Zoom Link</a> </p>$content";
         }
         return $content;
     }
